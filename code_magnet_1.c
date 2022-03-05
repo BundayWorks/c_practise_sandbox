f0ad8cea12949d53036eb0f38166bc27f6196d2d
Olabode Ogunfuye <olabode.ogunfuye@gmail.com>
2022-03-05 14:18:13 +0100

    Update on the code magnet exercise

--- code_magnet_1.c
+++ code_magnet_1.c
@@ -15,14 +15,20 @@ int main()
 		val = 10;
 	} else if (card_name[0] == 'Q'){
 		val = 10;
-	} else if (card_name[0] == 'A'){
-		val = 11;
 	} else if (card_name[0] == 'J'){
 		val = 10;
+	} else if (card_name[0] == 'A'){
+		val = 11;
 	} else {
 		val = atoi(card_name);
 	}
-	printf("The card value is: %i\n", val);
-	
+	/* Check if the value is 3 to 6 */
+	if ((val > 2) && (val < 7)) {
+		puts("Count has gone up");
+	}
+	/* Otherwise check if the card  was 10, J, Q or K */
+	else if (val == 10){
+		puts("Count has gone down");
+	}
 	return 0;
 }
