1ccc679a296c9d7bc089ab642af7981cc54a4ca1
Olabode Ogunfuye <olabode.ogunfuye@gmail.com>
2022-03-06 16:47:13 +0100

    largest number & loops

---
+++ largest_number.c
@@ -0,0 +1,19 @@
+/* 
+ * Determine the largest of two values
+ */
+
+#include <stdio.h>
+
+int larger(int a, int b)
+{
+	if (a > b)
+		return a;
+	return b;
+}
+
+int main()
+{
+	int greatest = larger(100, 1000);
+	printf("%i is the greatest!\n", greatest);
+	return 0;
+}
---
+++ loops_2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+	int counter;
+	for (counter = 1; counter < 11; counter++){
+		printf("%i green bottles, hanging on a wall\n", counter);
+	}
+}
